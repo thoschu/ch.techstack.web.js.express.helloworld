3cd975a86691272ecf8a21afe4e9eae66d0d257e
Tom S <thoschulte@gmail.com>
2018-10-17 22:37:26 +0200

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cluster = require('cluster'),
     path = require('path'),
     R = require('ramda'),
     router = require('./router.js'),
-    helper = require('./helpers/xpoweredby.middelware.js');
+    helper = require('./helpers/xpoweredby.middleware.js');
 
 if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running...`);
