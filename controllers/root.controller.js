3df22637a4f2e90d94d166efd4a69e5bad98b7d5
Thomas Schulte <thomas.schulte@plusserver.com>
2018-10-17 12:05:51 +0200

    cluster module added to take advantage of multi-core systems

--- controllers/root.controller.js
+++ controllers/root.controller.js
@@ -5,23 +5,20 @@ const fs = require('fs'),
 
 let rootAction = async (request, response) => {
     const pip = await Server.getPip().then(result => {return result}).catch(err => console.error(err)),
-        server = new Server(),
-        context = {
-            hostname: server.hostname,
-            pip
-        };
+        server = new Server();
+
+    const view = fs.readFileSync(path.join(__dirname, '../views/index.html'), 'utf8');
 
     fs.readFile(path.join(__dirname, '../views/index.html'), (err, data) => {
         if (err) {
-            console.error(err);
             response.status(500).send(err);
-        } else {
-            const template = handlebars.compile(data.toString()),
-                index = template(context);
-
-            response.set('Content-Type', 'text/html');
-            response.status(200).send(index);
         }
+
+        response.set('Content-Type', 'text/html');
+        response.status(200).send(handlebars.compile(view.toString())({
+            hostname: server.hostname,
+            pip
+        }));
     });
 };
 
--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const express = require('express'),
+const cluster = require('cluster'),
+    express = require('express'),
+    os = require('os'),
     path = require('path'),
     R = require('ramda'),
     router = require('./router.js'),
-    app = express(),
-    port = 80;
+    port = 3000;
 
-app.set('port', process.env.PORT || port);
-app.use(express.static(path.join(__dirname, 'public'), {
-    dotfiles: 'ignore',
-    etag: R.F(),
-    extensions: ['js', 'css'],
-    index: R.F(),
-    maxAge: '1d',
-    redirect: R.F(),
-    setHeaders: (res, path, stat) => {
-        res.set('x-timestamp', Date.now())
+if (cluster.isMaster) {
+    const numCPUs = R.length(os.cpus());
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-}));
 
-router(app);
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
 
-app.listen(app.get('port'), () => {
-    console.log(`Server listening on port ${app.get('port')}! Press Ctrl-C to terminate.`);
-});
+    app.set('port', process.env.PORT || port);
+    app.use(express.static(path.join(__dirname, 'public'), {
+        dotfiles: 'ignore',
+        etag: R.F(),
+        extensions: ['js', 'css'],
+        index: R.F(),
+        maxAge: '1d',
+        redirect: R.F(),
+        setHeaders: (res, path, stat) => {
+            res.set('x-timestamp', Date.now())
+        }
+    }));
+
+    router(app);
+
+    app.listen(app.get('port'), () => {
+        console.log(`Worker ${process.pid} started ↴`);
+        console.log(`Server listening on port ${app.get('port')}! Press Ctrl-C to terminate.`);
+    });
+}
