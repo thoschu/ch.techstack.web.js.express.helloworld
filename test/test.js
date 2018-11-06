12004ec17b040b89f506c6cdb3684e138fac023a
Thomas Schulte <thomas.schulte@plusserver.com>
2018-11-06 15:00:39 +0100

    test

--- test/test.js
+++ test/test.js
@@ -64,7 +64,7 @@ describe('Startpage', () => {
     describe('Headers', () => {
         it('x-powered-by should be express.js', (done) => {
             try {
-                expect(R.prop('x-powered-by', headers)).to.equal('express.js');
+                expect(R.prop('x-powered-by', headers)).to.equal('express.jss');
                 done();
             } catch (e) {
                 exitprocess(e);
